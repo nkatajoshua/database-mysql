4f7dce272bb29504f65ef211a4c3db24217186b3
nkatajoshua <nkatajoshua4@gmail.com>
2023-02-14 22:26:32 +0300

    Update trial1.sql

--- Prac 1/trial1.sql
+++ Prac 1/trial1.sql
@@ -9,4 +9,5 @@ PhoneNumber char(20)
 );
 insert into Customer (CustomerId, CustomerName, City, PhoneNumber) values (1,"Joshua","Chester","23773742");
 insert into Customer (CustomerId, CustomerName, City, PhoneNumber) values (2,"Joe","Hull","7843847437");
-insert into Customer (CustomerId, CustomerName, City, PhoneNumber) values (2,"Hank","Manchester","374892924");
+insert into Customer (CustomerId, CustomerName, City, PhoneNumber) values (4,"Hank","Manchester","374892924");
+insert into Customer (CustomerId, CustomerName, City, PhoneNumber) values (5,"Frank","Norwich","984389729");
